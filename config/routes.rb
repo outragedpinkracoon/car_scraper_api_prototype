3c7fac43a3186a182a806bd6608deb1bd9b498f0
Valerie Dryden <outragedpinkracoon@gmail.com>
2017-11-07 21:37:16 +0000

    adds default for route param

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get '/cars/used/:max', to: 'cars#used'
-
+  get '/cars/used', to: 'cars#used', defaults: { max: '5' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
